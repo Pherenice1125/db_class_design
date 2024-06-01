8f918ce3598e53ef69116b9fcf5a089a5f810f30
MysteryTianJi <MysteryTianJi@gmail.com>
2024-06-01 16:32:38 +0800

    The latest frontend files

---
+++ static/db_Config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 15:02:29 2024
+
+@author: Pherenice
+"""
+
+db_config = {
+    'user': 'root',
+    'password': '5472196',
+    'host': 'localhost',
+    'database': 'class_design'
+}
